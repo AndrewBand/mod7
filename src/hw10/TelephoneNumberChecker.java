2efce476bf15d18cc77b2de562af38d4e73c8a0a
AndrewBand <guitarist5@i.ua>
2022-06-11 17:57:24 +0300

    add TelephoneNumberChecker modification2

--- src/hw10/TelephoneNumberChecker.java
+++ src/hw10/TelephoneNumberChecker.java
@@ -8,7 +8,6 @@ import java.util.regex.Pattern;
 class TelephoneNumberChecker {
     public void numberChecker(String fileInName, String fileOutName) {
         ArrayList<String> ar = new ArrayList<String>();
-
         String line = null;
 
         File file = new File(fileInName);
@@ -17,22 +16,22 @@ class TelephoneNumberChecker {
             while ((line = br.readLine()) != null) {
                 if (!line.isEmpty() && !line.isBlank()) {
                     ar.add(line);
+
+                    System.out.println(line);
                 }
             }
         } catch (Exception e) {
         }
-        String regex = "[\\(|][\\d][\\d][\\d][\\-|[\\)\\ ]][\\d][\\d][\\d][\\-][\\d][\\d][\\d][\\d]";
+        String regex = "(.*)\\(?\\d{3}\\)?[\\s-]\\d{3}-\\d{4}(.*)";
         Pattern pattern = Pattern.compile(regex);
 
         File fileOut = new File(fileOutName);
 
         try (BufferedWriter bw = new BufferedWriter(new FileWriter(fileOut))) {
-
             for (int i = 0; i < ar.size(); i++) {
                 Matcher matcher = pattern.matcher(ar.get(i));
                 if (matcher.matches()) {
-                    System.out.println("ar.get(" + i + ") = " + ar.get(i));
-                    bw.write(ar.get(i));
+                    bw.write(ar.get(i) + "\n\n");
                 }
             }
         } catch (IOException e) {
