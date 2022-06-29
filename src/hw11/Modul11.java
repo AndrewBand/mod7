51226536dd1738a7c0d278d56814299525cdee33
AndrewBand <102629212+AndrewBand@users.noreply.github.com>
2022-06-29 21:43:20 +0300

    Update Modul11.java
    correct list of  java.util

--- src/hw11/Modul11.java
+++ src/hw11/Modul11.java
@@ -1,12 +1,6 @@
 package hw11;
 
-
-import edu.emory.mathcs.backport.java.util.Collections;
-
-import java.util.Arrays;
-import java.util.Comparator;
-import java.util.Iterator;
-import java.util.List;
+import java.util.*;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 import java.util.stream.Stream;
