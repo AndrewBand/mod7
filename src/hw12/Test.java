4a00d5d6fcd68dd07107956d98e4ed445da558c2
AndrewBand <102629212+AndrewBand@users.noreply.github.com>
2022-07-10 22:37:27 +0300

    Delete Test.java

--- src/hw12/Test.java
+++
@@ -1,9 +0,0 @@
-package hw12;
-
-public class Test {
-    public static void main(String[] args) {
-        System.out.println("System.nanoTime() = " + System.nanoTime());
-        System.out.println("System.currentTimeMillis() = " + System.currentTimeMillis());
-//        startTime
-    }
-}
